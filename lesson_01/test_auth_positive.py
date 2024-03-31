6fefc37a6bc1a1a1d91bbe4206b77f9528992f0f
den_kho <20597299+denkho@users.noreply.github.com>
2024-03-31 19:32:00 +0300

    lesson_01 cart tests part one plus gitignore

--- lesson_01/test_auth_positive.py
+++ lesson_01/test_auth_positive.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+
 url = 'https://www.saucedemo.com/'
 url_inventory = 'https://www.saucedemo.com/inventory.html'
 login = 'standard_user'
@@ -18,6 +19,7 @@ def test_authorisation_with_positive_credentials():
 
     assert driver.current_url == url_inventory, 'URL is incorrect'
 
+
 def test_authorisation_with_negative_credentials():
     driver.get(url)
     driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys(login_negative)
@@ -25,3 +27,5 @@ def test_authorisation_with_negative_credentials():
     driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
 
     assert driver.find_element(By.XPATH, '//*[@data-test="error"]').text == 'Epic sadface: Username and password do not match any user in this service'
+
+
