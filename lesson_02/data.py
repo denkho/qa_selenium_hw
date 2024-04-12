06997027dacc4aae4205a5ef5f31e2bb3a7f67bf
Denis <20597299+denkho@users.noreply.github.com>
2024-04-12 14:42:54 +0300

    lesson_02 test conftest, test authorization, data and locators (#2)

---
+++ lesson_02/data.py
@@ -0,0 +1,12 @@
+# Authorization
+
+login = 'standard_user'
+password = 'secret_sauce'
+login_negative = 'user'
+password_negative = 'user'
+
+
+# URLS
+
+url = 'https://www.saucedemo.com/'
+url_inventory = 'https://www.saucedemo.com/inventory.html'
---
+++ lesson_02/locators.py
@@ -0,0 +1,10 @@
+
+
+class MainPage:
+    username_field = '//*[@id="user-name"]'
+    password_field = '//*[@id="password"]'
+    login_button = '//*[@id="login-button"]'
+    login_error_message = '//*[@data-test="error"]'
+    login_error_message_text = 'Epic sadface: Username and password do not match any user in this service'
+
+
